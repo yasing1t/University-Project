349502752fd117a3bfc4181ab2ce104efab05af5
Amis <yasinkhosravi0499@gmail.com>
2023-05-08 14:54:57 +0800

    Create [currency_price] function

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import requests
 
 #-----------------------------------------------------------------------------------------------------------------#
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+def currency_price(currency) :
 
-currency = 'BTC' #Currency Symbol
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
-params = {
-    'symbol': currency,
-    'convert': 'USD'
-}
+    params = {
+        'symbol': currency, #Currency Symbol
+        'convert': 'USD'
+    }
 
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': 'ae42d262-3a62-41ef-86e5-af052ef8d5f5' #API KEY
-}
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': 'ae42d262-3a62-41ef-86e5-af052ef8d5f5' #API KEY
+    }
 
-response = requests.get(url, headers = headers, params = params)
-price = response.json()['data']['BTC']['quote']['USD']['price']
+    response = requests.get(url, headers = headers, params = params)
+    price = response.json()['data'][currency]['quote']['USD']['price']
+
+    return price
+
+#----------------------------------------------------------#
+
+currency = str(input('Enter currency symbol :'))
+print(currency_price(currency))
 
 #-----------------------------------------------------------------------------------------------------------------#
